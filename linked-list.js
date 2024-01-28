75bd111cf9ec9c103cda8ef4d2015102f1c2230d
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-01-28 17:56:13 +0100

    Set up the LinkedList and Node Classes

---
+++ linked-list.js
@@ -0,0 +1,67 @@
+'use strict'
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;     
+  }
+
+  append(value) {
+    
+
+    this.size++;
+  };
+
+  prepend(value) {
+    
+  };
+
+  size() {
+    return this.size();
+  }
+
+  head() {
+    
+  }
+
+  tail() {
+    
+  }
+
+  at(index) {
+    
+  }
+
+  pop() {
+    
+  }
+
+  contains(value) {
+    
+  }
+
+  find(value) {
+    
+  }
+
+  toString() {
+    
+  }
+
+  insertAt(value, index) {
+    
+  }
+
+  removeAt(index) (
+    
+  )
+  
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
